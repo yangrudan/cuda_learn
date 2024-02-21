42c672aaeb5340f71bb33c51221ad7a03273c326
Cookie <1549029765@qq.com>
2024-02-21 16:03:06 +0800

    1. add annotation; 2. save marked image

--- pytorch_learn/pic_io_csv.py
+++ pytorch_learn/pic_io_csv.py
@@ -16,14 +16,22 @@ from torchvision import transforms, utils
 import warnings
 warnings.filterwarnings("ignore", "\nPyarrow", DeprecationWarning)
 
+
+def show_landmarks(image, landmarks):
+    """显示带有地标的图片"""
+    plt.imshow(image)
+    plt.scatter(landmarks[:, 0], landmarks[:, 1], s=10, marker='.', c='r')
+    plt.pause(0.001) # pause a bit so that plots are updated
+
+
 plt.ion()
 # interactive mode
 
 landmarks_frame = pd.read_csv('./faces/face_landmarks.csv')
 n = 65  # N是特征点的数量
-img_name = landmarks_frame.iloc[n, 0]
+img_name = landmarks_frame.iloc[n, 0]  # iloc：通过整数位置获得行和列的数据。
 landmarks = landmarks_frame.iloc[n, 1:].to_numpy()
-landmarks = landmarks.astype('float').reshape(-1, 2)
+landmarks = landmarks.astype('float').reshape(-1, 2)  # It is quite simple, -1 means 'whatever it takes' to flatten. So, in the above example, a.reshape(2,-1) would mean 2*4, a.reshape(4,-1) would mean 4*2
 print('Image name: {}'.format(img_name))
 print('Landmarks shape: {}'.format(landmarks.shape))
 print('First 4 Landmarks: {}'.format(landmarks[:4]))
@@ -34,3 +42,10 @@ print('First 4 Landmarks: {}'.format(landmarks[:4]))
 #                     [33. 76.]
 # [34. 86.]
 # [34. 97.]]
+
+plt.figure()
+show_landmarks(io.imread(os.path.join('./faces/', img_name)),
+               landmarks)
+plt.show()
+plt.savefig("person-7-mark.jpg")
+
