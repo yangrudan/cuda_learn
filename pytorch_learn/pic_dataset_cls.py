48ff4177f93ff9856bbcb5585420b659f35cb894
Cookie <1549029765@qq.com>
2024-02-21 16:07:27 +0800

    建立数据集类

---
+++ pytorch_learn/pic_dataset_cls.py
@@ -0,0 +1,44 @@
+"""
+Copyright (c) Cookie Yang. All right reserved.
+"""
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+#用于更容易地进行csv解析
+from skimage import io, transform
+#用于图像的IO和变换
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+# 忽略警告
+import warnings
+warnings.filterwarnings("ignore", "\nPyarrow", DeprecationWarning)
+
+
+class FaceLandmarksDataset(Dataset):
+    """面部标记数据集."""
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        csv_file（string）：带注释的csv文件的路径。
+        root_dir（string）：包含所有图像的目录。
+        transform（callable， optional）：一个样本上的可用的可选变换
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir,
+                                self.landmarks_frame.iloc[idx, 0])
+        image = io.imread(img_name)
+        landmarks = self.landmarks_frame.iloc[idx, 1:]
+        landmarks = np.array([landmarks])
+        landmarks = landmarks.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'landmarks': landmarks}
+        if self.transform:
+            sample = self
