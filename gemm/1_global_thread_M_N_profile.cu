0b8094f92cde06c60eccda5e7010c1869ee05d56
Cookie <1549029765@qq.com>
2024-02-23 11:19:27 +0800

    profile actual not right

--- gemm/1_global_thread_M_N_profile.cu
+++ gemm/1_global_thread_M_N_profile.cu
@@ -168,3 +168,21 @@ float testPerformance(
 
     return sec;
 }
+/*
+ Kernal = naiveSgemm
+M N K =    128    128   1024, Time =   0.00009421   0.00009543   0.00010026 s, AVG Performance =   327.4748 Gflops
+M N K =    192    192   1024, Time =   0.00009418   0.00009502   0.00009526 s, AVG Performance =   739.9695 Gflops
+M N K =    256    256   1024, Time =   0.00009520   0.00009613   0.00009728 s, AVG Performance =  1300.2629 Gflops
+M N K =    384    384   1024, Time =   0.00018224   0.00018328   0.00018432 s, AVG Performance =  1534.5105 Gflops
+M N K =    512    512   1024, Time =   0.00035530   0.00035624   0.00035840 s, AVG Performance =  1403.5356 Gflops
+M N K =    768    768   1024, Time =   0.00071066   0.00071661   0.00071885 s, AVG Performance =  1569.8960 Gflops
+M N K =   1024   1024   1024, Time =   0.00115504   0.00115610   0.00115917 s, AVG Performance =  1729.9554 Gflops
+M N K =   1536   1536   1024, Time =   0.00261424   0.00266404   0.00267981 s, AVG Performance =  1689.1649 Gflops
+M N K =   2048   2048   1024, Time =   0.00426496   0.00450294   0.00466022 s, AVG Performance =  1776.6180 Gflops
+M N K =   3072   3072   1024, Time =   0.00952627   0.00955464   0.00956723 s, AVG Performance =  1883.9021 Gflops
+M N K =   4096   4096   1024, Time =   0.01682125   0.01690153   0.01704755 s, AVG Performance =  1893.3198 Gflops
+M N K =   6144   6144   1024, Time =   0.03800678   0.03812086   0.03818086 s, AVG Performance =  1888.7298 Gflops
+M N K =   8192   8192   1024, Time =   0.06766899   0.06783232   0.06795264 s, AVG Performance =  1887.0061 Gflops
+M N K =  12288  12288   1024, Time =   0.15264767   0.15344159   0.15378432 s, AVG Performance =  1876.9357 Gflops
+M N K =  16384  16384   1024, Time =   0.27192318   0.27260968   0.27276799 s, AVG Performance =  1878.1431 Gflops
+ */
